95b412463f1b74023ee4c5d628e7e2f61b41bc31
Brandon Greenwell <greenwell.brandon@gmail.com>
2017-10-23 13:25:53 -0400

    update

---
+++ R scripts/clams.R
@@ -0,0 +1,28 @@
+# Sample from intake site
+intake <- c(7.2, 7.5, 6.89, 6.95, 6.73, 7.25, 7.2, 6.85, 7.52, 7.01, 6.65, 7.55, 
+            7.14, 7.45, 7.24, 7.75, 6.85, 6.5, 6.64, 7.19, 7.15, 7.21, 7.15, 
+            7.3, 6.35)
+
+# Sample from discharge site
+discharge <- c(7.25, 7.23, 6.85, 7.07, 6.55, 7.43, 7.3, 6.9, 7.1, 6.95, 7.39, 
+               6.54, 6.39, 6.08, 6.3, 6.35, 7.34, 6.7, 7.08, 7.09, 7.4, 6, 6.94, 
+               5.95)
+
+# Histograms and boxplots
+par(mfrow = c(2, 2))
+hist(intake)
+hist(discharge)
+boxplot(intake, discharge, names = c("Intake", "Discharge"))
+boxplot(intake, discharge, names = c("Intake", "Discharge"), notch = TRUE)
+
+# Two-sample t-test assuming equal variances
+t.test(intake, discharge, alternative = "two.sided", 
+       mu = 0,
+       var.equal = TRUE,
+       conf.level = 0.95)
+
+# Two-sample t-test assuming unequal variances
+t.test(intake, discharge, alternative = "two.sided", 
+       mu = 0,
+       var.equal = FALSE,
+       conf.level = 0.95)
